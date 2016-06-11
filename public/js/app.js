e37dc6932c874b0d88347697d866c10ae24401bb
Gauthier Monserand <simkim@simkim.net>
2016-06-11 21:11:09 +0200

    Check file API

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
+
 function setupWS($scope, $timeout) {
   var protocol = "";
   if (document.location.protocol === "https:") {
